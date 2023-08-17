c57dfbd83975e8e06758b141b06728fd2e2a8b27
Arjun <arjun.prabhune@gmail.com>
2023-08-17 17:32:32 -0500

    "Second Fructus Commit"

---
+++ Fructus/App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Arjun Prabhune on 8/17/23.
+// New
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Arjun Prabhune on 8/17/23.
+// New
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
