d3abaaaa8a310d4c81a72a6901a5edafc8fdd34a
Arjun <arjun.prabhune@gmail.com>
2023-08-17 17:29:58 -0500

    "First Fructus Commit"

---
+++ Fructus/Views/FruitCardView.swift
@@ -0,0 +1,61 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Arjun Prabhune on 8/17/23.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - Properties
+    
+    
+    
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack {
+            VStack (spacing: 20){
+                // FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+
+                // FRUIT: TITLE
+                Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                // FRUIT: HEADLINE
+                Text("Blueberries are sweet, nutritious and wildly popular all around the world")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth:  480)
+                
+                
+                
+                
+                // BUTTON: Start
+            } //: VSTACK
+        } //: ZSTACK
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+        .ignoresSafeArea()
+    }
+}
+
+
+    // MARK: - Preview
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
---
+++ Fructus/Views/StartButtonView.swift
@@ -0,0 +1,27 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Arjun Prabhune on 8/17/23.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - Properties
+    
+    
+    // MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+// MARK: - Preview
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
