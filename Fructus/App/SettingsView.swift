024290212b09fb52da2f42d0adcf0ec0510fe092
Arjun <arjun.prabhune@gmail.com>
2023-09-02 12:58:34 -0500

    "Tenth Fructus Commit"

--- Fructus/App/SettingsView.swift
+++ Fructus/App/SettingsView.swift
@@ -12,7 +12,8 @@ struct SettingsView: View {
     // MARK: - Properties
     
     @Environment(\.presentationMode) var presentationMode
-    
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+ 
     // MARK: - Body
     
     var body: some View {
@@ -41,7 +42,35 @@ struct SettingsView: View {
                     
                     // MARK: - Section 2
                     
-                    
+                    GroupBox (
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish to restart your app to the onboarding screen, click this button!")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
+                    }
                     
                     // MARK: - Section 3
                     
